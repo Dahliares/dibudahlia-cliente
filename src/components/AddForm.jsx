1d5894b290d2dce22048d9b42fcfbcd136ecb2d8
Saraoz <roseofpain1@gmail.com>
2023-09-15 11:47:57 +0200

    Update AddForm.jsx

--- src/components/AddForm.jsx
+++ src/components/AddForm.jsx
@@ -1,10 +1,35 @@
 
-import '../components/ContactForm.css'
+import '../components/ContactForm.css';
+import { useRef } from 'react';
+import { useNavigate } from 'react-router-dom';
 
 
 export function AddForm() {
 
-   
+    const form = useRef(null);
+    const navigate = useNavigate();
+
+    const handleSubmit = (e) => {
+
+        e.preventDefault();
+    
+        let data = new FormData(form.current);
+    
+    
+        fetch('https://saraland-server.up.railway.app/add', {
+          method: 'POST',
+          body: data,
+    
+        })
+          .then(res => res.json())
+          .then(res => {
+            console.log(res);
+            alert("Dibujo guardado correctamente");
+            navigate('/');
+          })
+          .catch(err => console.log(err))
+    
+      }
 
 
     return (
@@ -15,7 +40,7 @@ export function AddForm() {
                     Sube un dibujo nuevo a tu base de datos
                 </div>
 
-                <form id="Addform" method='post' action='http://localhost:3333/add' enctype="multipart/form-data" className="form">
+                <form id="Addform" ref={form} encType="multipart/form-data" className="form">
 
                     <label htmlFor="name">Nombre:</label>
                     <input type="text" name="name" id="name" required />
@@ -29,7 +54,7 @@ export function AddForm() {
                     <label htmlFor="file">Imagen:</label>
                     <input type='file' name='file' id='file' />
 
-                    <input type="submit" id="button" value="Añadir dibujo" />
+                    <input type="submit" id="button" value="Añadir dibujo" onClick={handleSubmit}/>
                 </form>
             </div>
 
